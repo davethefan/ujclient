bf37fd90d89691756cae3525b60ded69e7cba523
David Thefan <david.thefan@gmail.com>
2017-03-16 02:21:31 +0000

    added comment

--- dev/login-requests-simple.py
+++ dev/login-requests-simple.py
@@ -10,4 +10,5 @@ with requests.Session() as c:
 	c.post('https://authenticate.gateway.gov.uk/sidp/SignIn.ashx?gwv=1.0&gwrealm=urn%3aTransformingLabourMarketServices&gwtheme=directgov&gwcategory=Ind', data=payload)
 	r = c.get('https://jobsearch.direct.gov.uk/Home.aspx')
 	
+	#search for the word 'logout' in the body text, to return whether a user is logged in or not.
 	print 'Logged in?:', 'Logout' in r.content
