6aae894cf3047aa4eada253fbc8823accbb087f3
David Thefan <david.thefan@gmail.com>
2017-03-16 02:10:38 +0000

    UI friendlification

--- dev/login-requests-simple.py
+++ dev/login-requests-simple.py
@@ -9,6 +9,4 @@ with requests.Session() as c:
 	c.post('https://authenticate.gateway.gov.uk/sidp/SignIn.ashx?gwv=1.0&gwrealm=urn%3aTransformingLabourMarketServices&gwtheme=directgov&gwcategory=Ind', data=payload)
 	r = c.get('https://jobsearch.direct.gov.uk/Home.aspx')
 	
-	print ('Login successful?')
-	print 'Logout' in r.content 
-	
+	print 'Logged in?:', 'Logout' in r.content
