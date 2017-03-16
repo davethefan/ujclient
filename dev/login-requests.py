86472cb0908bf07758f73e9a6cda69d78050c708
David Thefan <david.thefan@gmail.com>
2017-03-16 01:06:10 +0000

    added requests method for login

--- dev/login-requests.py
+++ dev/login-requests.py
@@ -5,7 +5,8 @@ with requests.Session() as c:
 	PASSWORD = "password"
 	c.get(url)
 	csrftoken = c.cookies['csrftoken']
-	logindata = dict(csrfmiddlewaretoken=csrftoken,username=USERNAME, password=PASSWORD, next="https://jobsearch.direct.gov.uk/Home.aspx")
+	logintoken = c.cookies['login_token']
+	logindata = dict(csrfmiddlewaretoken=csrftoken, username=USERNAME, password=PASSWORD, logintoken=login_token, next="https://jobsearch.direct.gov.uk/Home.aspx")
 	c.post(url,data=logindata,headers={'Referer':'https://authenticate.gateway.gov.uk/sidp/SignIn.ashx?gwv=1.0&gwrealm=urn%3aTransformingLabourMarketServices&gwtheme=directgov&gwcategory=Ind'})
 	page=c.get('https://jobsearch.direct.gov.uk/Home.aspx')
 	print (page.content)
